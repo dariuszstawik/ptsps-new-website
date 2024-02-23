2a57129ebddaaf8843e67f0912b5ab6ec9421cca
Dariusz Stawik <dariusz.stawik@gmail.com>
2024-02-23 20:41:27 +0100

    Update supervisors map

--- components/superwizja-page/supervisors-map/index.js
+++ components/superwizja-page/supervisors-map/index.js
@@ -1,54 +1,213 @@
 "use client";
 
-import { MapPin } from "lucide-react";
+import { Mail, MapPin, Phone } from "lucide-react";
+import { useState } from "react";
 
 export default function SupervisorsMap({ content }) {
-  const longitudeWarszawa = 21.012;
-  const latitudeWarszawa = 52.229;
+  const [region, setRegion] = useState("");
+  const [city, setCity] = useState("");
 
-  const longitudeKatowice = 19.023;
-  const latitudeKatowice = 50.264;
+  const changeRegion = (e) => {
+    setRegion(e.target.value);
+    setCity("");
+  };
 
-  const longitudeOnMapKatowice =
-    (((longitudeKatowice - 14.12) / 10.03 - 0.02) * 100).toFixed(2).toString() +
-    "%";
+  const changeCity = (e) => {
+    setCity(e.target.value);
+    setRegion("");
+  };
 
-  const latitudeOnMapKatowice =
-    (((latitudeKatowice - 49.0) / 5.83) * 100).toFixed(2).toString() + "%";
+  const changeCityThroughMapPin = (city) => {
+    setCity(city);
+    setRegion("");
+  };
 
-  const longitudeOnMapWarszawa =
-    (((longitudeWarszawa - 14.12) / 10.03 - 0.02) * 100).toFixed(2).toString() +
-    "%";
+  // const longitudeWarszawa = 21.012;
+  // const latitudeWarszawa = 52.229;
 
-  const latitudeOnMapWarszawa =
-    (((latitudeWarszawa - 49.0) / 5.83) * 100).toFixed(2).toString() + "%";
+  // const longitudeKatowice = 19.023;
+  // const latitudeKatowice = 50.264;
+
+  // const longitudeOnMapKatowice =
+  //   (((longitudeKatowice - 14.12) / 10.03 - 0.02) * 100).toFixed(2).toString() +
+  //   "%";
+
+  // const latitudeOnMapKatowice =
+  //   (((latitudeKatowice - 49.0) / 5.83) * 100).toFixed(2).toString() + "%";
+
+  // const longitudeOnMapWarszawa =
+  //   (((longitudeWarszawa - 14.12) / 10.03 - 0.02) * 100).toFixed(2).toString() +
+  //   "%";
+
+  // const latitudeOnMapWarszawa =
+  //   (((latitudeWarszawa - 49.0) / 5.83) * 100).toFixed(2).toString() + "%";
 
   console.log(content);
   // console.log(content.fields.location.lat, content[0].fields.location.lon);
 
+  console.log(region, city);
+
   return (
-    <div className="bg-white w-[600px] relative">
-      <img
-        src="/mappol1.svg"
-        alt="Mapa superwizorów"
-        className="w-full h-full"
-      />
-      <MapPin
-        // style={`{ position: "absolute", top: "${latitudeOnMap}", left: "${longitudeOnMap}" }`}
-        style={{
-          position: "absolute",
-          bottom: latitudeOnMapKatowice,
-          left: longitudeOnMapKatowice,
-        }}
-      />
-      <MapPin
-        // style={`{ position: "absolute", top: "${latitudeOnMap}", left: "${longitudeOnMap}" }`}
-        style={{
-          position: "absolute",
-          bottom: latitudeOnMapWarszawa,
-          left: longitudeOnMapWarszawa,
-        }}
-      />
+    <div className="w-full flex gap-32">
+      <div className="bg-white w-[600px] relative">
+        <img
+          src="/mappol1.svg"
+          alt="Mapa superwizorów"
+          className="w-full h-full"
+        />
+
+        <ul>
+          {content.map((supervisor) => (
+            <li
+              key={supervisor.sys.id}
+              style={{
+                position: "absolute",
+                bottom:
+                  (
+                    ((supervisor.fields.location.lat - 49.0) / 5.83) *
+                    100
+                  ).toFixed(2) + "%",
+                left:
+                  (
+                    ((supervisor.fields.location.lon - 14.12) / 10.03 - 0.02) *
+                    100
+                  ).toFixed(2) + "%",
+              }}
+            >
+              <MapPin
+                onClick={() => changeCityThroughMapPin(supervisor.fields.city)}
+                className={`${
+                  supervisor.fields.city === city && "text-primaryYellow"
+                } cursor-pointer`}
+              />
+            </li>
+          ))}
+        </ul>
+
+        {/* <MapPin
+          // style={`{ position: "absolute", top: "${latitudeOnMap}", left: "${longitudeOnMap}" }`}
+          style={{
+            position: "absolute",
+            bottom: latitudeOnMapKatowice,
+            left: longitudeOnMapKatowice,
+          }}
+        />
+        <MapPin
+          // style={`{ position: "absolute", top: "${latitudeOnMap}", left: "${longitudeOnMap}" }`}
+          style={{
+            position: "absolute",
+            bottom: latitudeOnMapWarszawa,
+            left: longitudeOnMapWarszawa,
+          }}
+        /> */}
+      </div>
+      <div>
+        <select
+          onChange={changeRegion}
+          value={region}
+          className="border border-primaryBlue rounded-md p-2"
+        >
+          <option value="">Wszystkie</option>
+          <option value="dolnośląskie">Dolnośląskie</option>
+          <option value="kujawsko-pomorskie">Kujawsko-pomorskie</option>
+          <option value="lubelskie">Lubelskie</option>
+          <option value="lubuskie">Lubuskie</option>
+          <option value="łódzkie">Łódzkie</option>
+          <option value="małopolskie">Małopolskie</option>
+          <option value="mazowieckie">Mazowieckie</option>
+          <option value="opolskie">Opolskie</option>
+          <option value="podkarpackie">Podkarpackie</option>
+          <option value="podlaskie">Podlaskie</option>
+          <option value="pomorskie">Pomorskie</option>
+          <option value="śląskie">Śląskie</option>
+          <option value="świętokrzyskie">Świętokrzyskie</option>
+          <option value="warmińsko-mazurskie">Warmińsko-mazurskie</option>
+          <option value="wielkopolskie">Wielkopolskie</option>
+          <option value="zachodniopomorskie">Zachodniopomorskie</option>
+        </select>
+
+        <select
+          onChange={changeCity}
+          value={city}
+          className="border border-primaryBlue rounded-md p-2"
+        >
+          <option value="">Wszystkie</option>
+          {content.map((supervisor) => (
+            <option key={supervisor.sys.id} value={supervisor.fields.city}>
+              {supervisor.fields.city}
+            </option>
+          ))}
+        </select>
+
+        <ul>
+          {/* {content
+            .filter((supervisor) =>
+              // region
+              //   ? region.toUpperCase() ===
+              //     supervisor.fields.region.toUpperCase()
+              //   : city.toUpperCase() === supervisor.fields.city.toUpperCase()
+              switch (true) {
+                case region:
+                  return region.toUpperCase() === supervisor.fields.region.toUpperCase();
+                case city:
+                  return city.toUpperCase() === supervisor.fields.city.toUpperCase();
+                default:
+                  return supervisor;
+              }
+              
+            ) */}
+          {/* {content
+            .filter((supervisor) => {
+              switch (true) {
+                case region:
+                  return (
+                    region.toUpperCase() ===
+                    supervisor.fields.region.toUpperCase()
+                  );
+                case city:
+                  return (
+                    city.toUpperCase() === supervisor.fields.city.toUpperCase()
+                  );
+                default:
+                  return true; // Return true for supervisors that don't match region or city
+              }
+            }) */}
+          {content
+            .filter((supervisor) => {
+              if (region) {
+                return (
+                  region.toUpperCase() ===
+                  supervisor.fields.region.toUpperCase()
+                );
+              } else if (city) {
+                return (
+                  city.toUpperCase() === supervisor.fields.city.toUpperCase()
+                );
+              } else {
+                return true; // Jeśli ani region, ani city nie jest zdefiniowane, odfiltruj wszystko
+              }
+            })
+            .map((supervisor) => (
+              <li key={supervisor.sys.id}>
+                <p>{supervisor.fields.name}</p>
+                <p className="flex gap-2">
+                  <MapPin className="text-primaryBlue w-5" />
+                  {supervisor.fields.city}
+                </p>
+                <p className="flex gap-2">
+                  {" "}
+                  <Mail className="text-primaryBlue w-5" />{" "}
+                  {supervisor.fields.email}
+                </p>
+                <p className="flex gap-2">
+                  {" "}
+                  <Phone className="text-primaryBlue w-4" />{" "}
+                  {supervisor.fields.phone}
+                </p>
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
