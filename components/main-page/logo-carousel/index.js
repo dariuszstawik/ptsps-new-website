f187e1244ed357f6f1d473a765b131746c6b854e
Dariusz Stawik <dariusz.stawik@gmail.com>
2024-04-18 15:35:21 +0200

    Update logo carousel

--- components/main-page/logo-carousel/index.js
+++ components/main-page/logo-carousel/index.js
@@ -29,97 +29,127 @@ export default function LogoCarousel() {
       <div className="hidden lg:block">
         <Slider {...settingsLg}>
           <div className="w-56 h-56  flex content-center p-8">
-            <img
-              src="/logo-unicef.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[75%]"
-            />
+            <a href="https://unicef.pl/" target="_blank">
+              <img
+                src="/logo-unicef.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[75%]"
+              />
+            </a>
           </div>
 
           <div className="w-56 h-56 flex content-center p-8">
-            <img
-              src="/logo-scsps.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[85%]"
-            />
+            <a href="https://www.facebook.com/superwizorzy/" target="_blank">
+              <img
+                src="/logo-scsps.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[85%]"
+              />
+            </a>
           </div>
 
           <div className="w-56 h-56 flex content-center p-8">
-            <img
-              src="/logo-superwizornia.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[85%]"
-            />
+            <a href="https://www.facebook.com/Superwizornia/" target="_blank">
+              <img
+                src="/logo-superwizornia.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[85%]"
+              />
+            </a>
           </div>
           <div className="w-56 h-56 flex content-center p-8">
-            <img
-              src="/logo-pracownia-superwizji.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[85%]"
-            />
+            <a
+              href="https://www.superwizjapomocyspolecznej.pl/"
+              target="_blank"
+            >
+              <img
+                src="/logo-pracownia-superwizji.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[85%]"
+              />
+            </a>
           </div>
           <div className="w-56 h-56 flex content-center p-8">
-            <img
-              src="/logo-pfz.png"
-              alt="logo Unicef"
-              className="mx-auto max-h-[85%]"
-            />
+            <a href="https://federacja-socjalnych.pl/" target="_blank">
+              <img
+                src="/logo-pfz.png"
+                alt="logo Unicef"
+                className="mx-auto max-h-[85%]"
+              />
+            </a>
           </div>
           <div className="w-56 h-56  flex content-center p-8">
-            <img
-              src="/logo-osar.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[80%]"
-            />
+            <a href="https://www.osar.com.pl/" target="_blank">
+              <img
+                src="/logo-osar.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[80%]"
+              />
+            </a>
           </div>
         </Slider>
       </div>
 
       <div className="block lg:hidden">
         <Slider {...settingsSm}>
-          <div className="w-56 h-56 flex content-center p-8">
-            <img
-              src="/logo-unicef.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[75%]"
-            />
+          <div className="w-56 h-56  flex content-center p-8">
+            <a href="https://unicef.pl/" target="_blank">
+              <img
+                src="/logo-unicef.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[75%]"
+              />
+            </a>
           </div>
 
           <div className="w-56 h-56 flex content-center p-8">
-            <img
-              src="/logo-scsps.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[85%]"
-            />
+            <a href="https://www.facebook.com/superwizorzy/" target="_blank">
+              <img
+                src="/logo-scsps.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[85%]"
+              />
+            </a>
           </div>
 
           <div className="w-56 h-56 flex content-center p-8">
-            <img
-              src="/logo-superwizornia.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[85%]"
-            />
+            <a href="https://www.facebook.com/Superwizornia/" target="_blank">
+              <img
+                src="/logo-superwizornia.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[85%]"
+              />
+            </a>
           </div>
           <div className="w-56 h-56 flex content-center p-8">
-            <img
-              src="/logo-pracownia-superwizji.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[85%]"
-            />
+            <a
+              href="https://www.superwizjapomocyspolecznej.pl/"
+              target="_blank"
+            >
+              <img
+                src="/logo-pracownia-superwizji.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[85%]"
+              />
+            </a>
           </div>
           <div className="w-56 h-56 flex content-center p-8">
-            <img
-              src="/logo-pfz.png"
-              alt="logo Unicef"
-              className="mx-auto max-h-[85%]"
-            />
+            <a href="https://federacja-socjalnych.pl/" target="_blank">
+              <img
+                src="/logo-pfz.png"
+                alt="logo Unicef"
+                className="mx-auto max-h-[85%]"
+              />
+            </a>
           </div>
           <div className="w-56 h-56  flex content-center p-8">
-            <img
-              src="/logo-osar.jpg"
-              alt="logo Unicef"
-              className="mx-auto max-h-[80%]"
-            />
+            <a href="https://www.osar.com.pl/" target="_blank">
+              <img
+                src="/logo-osar.jpg"
+                alt="logo Unicef"
+                className="mx-auto max-h-[80%]"
+              />
+            </a>
           </div>
         </Slider>
       </div>
