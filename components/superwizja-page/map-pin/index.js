55ceed79a9e129fdca545604457c5529500c70d2
Dariusz Stawik <dariusz.stawik@gmail.com>
2024-07-04 21:24:53 +0200

    Update supervisors map

--- components/superwizja-page/map-pin/index.js
+++ components/superwizja-page/map-pin/index.js
@@ -1,10 +1,18 @@
-export default function MapPin2({ className, onClick }) {
+export default function MapPin2({
+  className,
+  onClick,
+  onMouseEnter,
+  onMouseLeave,
+}) {
   return (
     <img
       src="/map-pin2.svg"
       alt="Map pin"
       className={`w-4  ${className}`}
       onClick={onClick}
+      // onHover={onHover}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     />
   );
 }
--- components/superwizja-page/supervisors-map/index.js
+++ components/superwizja-page/supervisors-map/index.js
@@ -7,6 +7,7 @@ import { useState } from "react";
 import MapPin2 from "../map-pin";
 
 export default function SupervisorsMap({ content }) {
+  const [isHovered, setIsHovered] = useState(null);
   const [region, setRegion] = useState("");
   const [city, setCity] = useState("");
 
@@ -24,6 +25,10 @@ export default function SupervisorsMap({ content }) {
     setCity(city);
     setRegion("");
   };
+
+  // const handleHover = () => {
+  //   setIsHovered(!isHovered);
+  // };
 
   // const longitudeWarszawa = 21.012;
   // const latitudeWarszawa = 52.229;
@@ -45,11 +50,12 @@ export default function SupervisorsMap({ content }) {
   // const latitudeOnMapWarszawa =
   //   (((latitudeWarszawa - 49.0) / 5.83) * 100).toFixed(2).toString() + "%";
 
-  console.log(content);
+  // console.log(content);
   // console.log(content.fields.location.lat, content[0].fields.location.lon);
+  console.log(isHovered);
 
   return (
-    <div className="w-full flex  gap-20">
+    <div className="w-full flex gap-20">
       <div className="bg-white h-[80vh] relative mb-auto shrink-0">
         <img
           src="/mappol1.svg"
@@ -75,12 +81,27 @@ export default function SupervisorsMap({ content }) {
                   ).toFixed(2) + "%",
               }}
             >
-              <MapPin2
+              <div
                 onClick={() => changeCityThroughMapPin(supervisor.fields.city)}
-                className={`${
-                  supervisor.fields.city === city && "text-primaryYellow"
-                } cursor-pointer`}
-              />
+                onMouseEnter={() => setIsHovered(supervisor.sys.id)}
+                onMouseLeave={() => setIsHovered(null)}
+                className="relative"
+              >
+                <MapPin2
+                  // onMouseEnter={() => setIsHovered(supervisor.sys.id)}
+                  // onMouseLeave={() => setIsHovered(null)}
+                  className={`${
+                    supervisor.fields.city === city && "text-primaryYellow"
+                  } cursor-pointer
+
+                  `}
+                />
+                {isHovered === supervisor.sys.id && (
+                  <div className="absolute -top-14 left-0 bg-white p-1 text-sm rounded">
+                    {supervisor.fields.city}
+                  </div>
+                )}
+              </div>
             </li>
           ))}
         </ul>
@@ -103,42 +124,54 @@ export default function SupervisorsMap({ content }) {
         /> */}
       </div>
       <div>
-        <select
-          onChange={changeRegion}
-          value={region}
-          className="border border-primaryBlue rounded-md p-2"
-        >
-          <option value="">Wszystkie</option>
-          <option value="dolnośląskie">Dolnośląskie</option>
-          <option value="kujawsko-pomorskie">Kujawsko-pomorskie</option>
-          <option value="lubelskie">Lubelskie</option>
-          <option value="lubuskie">Lubuskie</option>
-          <option value="łódzkie">Łódzkie</option>
-          <option value="małopolskie">Małopolskie</option>
-          <option value="mazowieckie">Mazowieckie</option>
-          <option value="opolskie">Opolskie</option>
-          <option value="podkarpackie">Podkarpackie</option>
-          <option value="podlaskie">Podlaskie</option>
-          <option value="pomorskie">Pomorskie</option>
-          <option value="śląskie">Śląskie</option>
-          <option value="świętokrzyskie">Świętokrzyskie</option>
-          <option value="warmińsko-mazurskie">Warmińsko-mazurskie</option>
-          <option value="wielkopolskie">Wielkopolskie</option>
-          <option value="zachodniopomorskie">Zachodniopomorskie</option>
-        </select>
-
-        <select
-          onChange={changeCity}
-          value={city}
-          className="border border-primaryBlue rounded-md p-2"
-        >
-          <option value="">Wszystkie</option>
-          {content.map((supervisor) => (
-            <option key={supervisor.sys.id} value={supervisor.fields.city}>
-              {supervisor.fields.city}
-            </option>
-          ))}
-        </select>
+        <div className="mx-auto mb-4">
+          Aby wyświetlić listę superwizorów dostępnych w danej lokalizacji,
+          kliknij na pinezkę na mapie lub wybierz poniżej odpowiednie
+          województwo lub miasto.
+        </div>
+        <div className="flex gap-4">
+          <div>
+            <h3 className="text-lg mt-4 mb-2  font-semibold">Województwo </h3>
+            <select
+              onChange={changeRegion}
+              value={region}
+              className="border border-primaryBlue rounded-md p-2"
+            >
+              <option value="">Wszystkie</option>
+              <option value="dolnośląskie">Dolnośląskie</option>
+              <option value="kujawsko-pomorskie">Kujawsko-pomorskie</option>
+              <option value="lubelskie">Lubelskie</option>
+              <option value="lubuskie">Lubuskie</option>
+              <option value="łódzkie">Łódzkie</option>
+              <option value="małopolskie">Małopolskie</option>
+              <option value="mazowieckie">Mazowieckie</option>
+              <option value="opolskie">Opolskie</option>
+              <option value="podkarpackie">Podkarpackie</option>
+              <option value="podlaskie">Podlaskie</option>
+              <option value="pomorskie">Pomorskie</option>
+              <option value="śląskie">Śląskie</option>
+              <option value="świętokrzyskie">Świętokrzyskie</option>
+              <option value="warmińsko-mazurskie">Warmińsko-mazurskie</option>
+              <option value="wielkopolskie">Wielkopolskie</option>
+              <option value="zachodniopomorskie">Zachodniopomorskie</option>
+            </select>
+          </div>
+          <div className="">
+            <h3 className="text-lg mt-4 mb-2 font-semibold">Miasto </h3>
+            <select
+              onChange={changeCity}
+              value={city}
+              className="border border-primaryBlue rounded-md p-2"
+            >
+              <option value="">Wszystkie</option>
+              {content.map((supervisor) => (
+                <option key={supervisor.sys.id} value={supervisor.fields.city}>
+                  {supervisor.fields.city}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
 
         <ul>
           {/* {content
@@ -189,14 +222,11 @@ export default function SupervisorsMap({ content }) {
               }
             })
             .map((supervisor) => (
-              <li
-                key={supervisor.sys.id}
-                className="flex gap-10 items-center mt-8"
-              >
-                <img
+              <li key={supervisor.sys.id} className="flex items-center mt-6">
+                {/* <img
                   src={supervisor.fields.image?.fields.file.url}
-                  className="w-40 h-40 rounded-full object-cover"
-                />
+                  className="w-24 h-24 rounded-full object-cover"
+                /> */}
                 <div className="pb-2">
                   <p className="font-semibold text-xl text-primaryBlue">
                     {supervisor.fields.name}
@@ -205,7 +235,7 @@ export default function SupervisorsMap({ content }) {
                     <MapPin className="text-primaryBlue w-5" />
                     {supervisor.fields.city}
                   </p>
-                  <p className="flex gap-2">
+                  {/* <p className="flex gap-2">
                     {" "}
                     <Mail className="text-primaryBlue w-5" />{" "}
                     {supervisor.fields.email}
@@ -214,14 +244,15 @@ export default function SupervisorsMap({ content }) {
                     {" "}
                     <Phone className="text-primaryBlue w-4" />{" "}
                     {supervisor.fields.phone}
-                  </p>
+                  </p> */}
                   <Link
                     href={`/superwizja/lista-superwizorow/${supervisor.fields.slug}`}
                   >
                     <button className="text-primaryBlue font-semibold">
-                      czytaj więcej &rarr;
+                      zobacz profil &rarr;
                     </button>
                   </Link>
+                  <hr className="mt-4" />
                 </div>
               </li>
             ))}
